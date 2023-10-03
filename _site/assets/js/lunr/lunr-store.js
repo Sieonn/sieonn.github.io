6927b791308f9278dc4df51d1dd36b4071b6c22a
Sieon <sieon.eve@gmail.com>
2023-10-03 15:39:04 +0900

    zz

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,67 @@
+var store = [{
+        "title": "0110",
+        "excerpt":"1. 화면에 Hello World 문자열을 출력하세요.           정답         print('Hello World')             2. 화면에 Mary's cosmetics 을 출력하세요.           정답         print(\"Mary's cometics\")             3. 화면에 아래 문장을 출력하세요.     신씨가 소리 질렀다. “도둑이야”.            정답         print('신씨가 소리 질렀다. \"도둑이야')             4. 화면에 C:\\Windows 을 출력하세요.           정답         print(\"C:\\Windows\")             5. 다음 코드를 실행해보고 \\t와 \\n의 역할을 설명해보세요.   print(\"안녕하세요.\\n만나서\\t\\t반갑습니다.\")           정답         안녕하세요.   만나서      반갑습니다.             6. print 함수에 두 개의 단어를 입력한 예제입니다. 아래 코드의 출력 결과를 예상해봅시다.   print (\"오늘은\", \"일요일\")           정답          💡 여러 값을 출력 가능하다.    쉼표로 구별해주면 된다. ⇒ 결과: 오늘은 일요일                7. print() 함수를 사용하여 다음과 같이 출력하세요.     naver;kakao;sk;samsung            정답         print(\"naver;kakao;sk;samsung\")           💡other sol         print(\"naver\",\"kakao\",\"sk\",\"samsung\", sep=\";\")             8. print() 함수를 사용하여 다음과 같이 출력하세요.     naver/kakao/sk/samsung            정답         print(\"naver\",\"kakao\",\"sk\",\"samsung\", sep=\"/\")             9. 다음 코드를 수정하여 줄바꿈이 없이 출력하세요.    print(\"first\");print(\"second\")           정답         print(\"first\", end=\"\");print(\"second\")             10. 5/3의 결과를 화면에 출력하세요.           정답         print(5/3)            ","categories": [],
+        "tags": [],
+        "url": "/0110/",
+        "teaser": null
+      },{
+        "title": "기초 300제",
+        "excerpt":" ","categories": ["samback"],
+        "tags": [],
+        "url": "/samback/%EA%B8%B0%EC%B4%88-300%EC%A0%9C-main/",
+        "teaser": null
+      },{
+        "title": "01.기초",
+        "excerpt":" ","categories": ["one"],
+        "tags": [],
+        "url": "/one/300%EC%A0%9C-1/",
+        "teaser": null
+      },{
+        "title": "첫 포스팅 입니다. 설레네유",
+        "excerpt":"오늘 처음 블로그를 만들었어요.   앞으로 꾸준히 써야겠어용.   너무 힘든 하루였다.   힘들다… 푹 자고싶어..     ","categories": [],
+        "tags": [],
+        "url": "/first/",
+        "teaser": null
+      },{
+        "title": "Python 공부 기록",
+        "excerpt":"##Json이란?&lt;\\n&gt; json의 함수이름을 알아 봅시다. s가 안 붙은게 함수다. dump는 직렬화하는 함수이다.   ####json - JSON 인코더와 디코더   인코더는 직렬화(Serialization)   사용하는 자료형을 문자열화 하는 것   디코더는 역직렬화 (Deserialization)   문자열화 된 것을 다시 자료형으로 만드는 것.   s가 붙으면 데이터 처리 s가 안 붙어있으면 파일을 처리를 합니다. json.load()f], *) load는 역직렬화 시키는 것 입니다. 객체를 string으로 바꿔줍니다.   ","categories": ["python"],
+        "tags": [],
+        "url": "/python/second/",
+        "teaser": null
+      },{
+        "title": "Python 공부 기록",
+        "excerpt":"https://seungwubaek.github.io/blog/whole_toc/#page-title   이미지 목차   이미지 세부 목차1   이미지입니다   이미지 세부 목차2   이미지 입니다.   이미지 세부 목차3   이미지 입니다.  ","categories": [],
+        "tags": [],
+        "url": "/%EC%A0%95%EB%A6%AC/",
+        "teaser": null
+      },{
+        "title": "블로그 만들기",
+        "excerpt":"블로그를 찾아서..   기존에 기록용으로 Notion을 사용했다. UI도 깔끔하고 작성하기 좋았으나 제대로된 블로그를 만들어보라는 권유를 받고 내가 공부하는것들을 기록할 수 있는 최적의 플랫폼을 찾아서 떠돌게 됐다.   Tistory? velog? Naver?   나는 우선 네이버 블로그를 오래 전부터 메모장같이 사용해왔다.편한 기능과 깔끔한 디자인이 좋았지만 커스텀의 한계가 있었다. velog와 Tistory도 너무 좋은 플랫폼인데 하나씩 신경쓰였다. ‘‘이것만 이랬어도…’ 사용하면서 자꾸 이런 생각에 시간만 뺏겼다.   없으면 만들어야지   그러다 GitBlog를 발견했다. 제대로 개발도 못하는 내가 얘를 잘 다룰 수 있을지 고민이 됐지만, 더 이상 떠돌기에는 시간이 아까웠고 GitHub Blog를 개성있게 자기가 원하는데로 꾸민 여러 블로그를 보고나니까 결심이 섰다. 깨지고 부셔지면서 배우지 뭐…   ","categories": ["blog"],
+        "tags": [],
+        "url": "/blog/GitHub-Blog-GitHub%EB%B8%94%EB%A1%9C%EA%B7%B8-%EC%A0%95%EC%B0%A9%EA%B8%B0-%EC%8B%9C%EC%9E%91/",
+        "teaser": null
+      },{
+        "title": "[프로그래머스]] 콜라츠 수열 만들기",
+        "excerpt":"콜라츠 수열 만들기   문제 설명       모든 자연수 x에 대해서 현재 값이 x이면 x가 짝수일 때는 2로 나누고, x가 홀수일 때는 3 * x + 1로 바꾸는 계산을 계속해서 반복하면 언젠가는 반드시 x가 1이 되는지 묻는 문제를 콜라츠 문제라고 부릅니다.그리고 위 과정에서 거쳐간 모든 수를 기록한 수열을 콜라츠 수열이라고 부릅니다.계산 결과 1,000 보다 작거나 같은 수에 대해서는 전부 언젠가 1에 도달한다는 것이 알려져 있습니다.임의의 1,000 보다 작거나 같은 양의 정수 n이 주어질 때 초기값이 n인 콜라츠 수열을 return 하는 solution 함수를 완성해 주세요.      제한사항      1 ≤ n ≤ 1,000     입출력 예                  n       result                       10       [10, 5, 16, 8, 4, 2, 1]           def solution(n):      s = []      while n != 0:          if n % 2 == 0:              s.append(n)              n //= 2          else:              s.append(n)              n = 3*n + 1          if (n == 1):              s.append(n)               break      return s  ","categories": ["programmers"],
+        "tags": [],
+        "url": "/programmers/%EC%BD%9C%EB%9D%BC%EC%B8%A0-%EC%88%98%EC%97%B4-%EB%A7%8C%EB%93%A4%EA%B8%B0/",
+        "teaser": null
+      },{
+        "title": "[프로그래머스]간단한 논리 연산",
+        "excerpt":"문서설명     boolean 변수 x1,x2,x3,x4가 매개변수로 주어질 때, 다음의 식의 true/false를 return하는 solution 함수를 작성해 주세요.         (x1∨ x2) ∧ (x3 ∨ x4 )      And, or 논리 연산이라고 생각하면 된다. ∨는 or 연산으로 둘 중에 하나만 참이여도 True이다. ∧는 and 연산으로 둘 다 참인 경우만 True를 반환한다.   def solution(x1, x2, x3, x4):     answer = False     if (x1+x2) and (x3+x4) &gt;=1:         answer = True     elif (x1+x2) or (x3+x4) ==0:         answer = False     elif (x1+x2) and (x3+x4) ==0:         answer = False     return answer   다른 답  def solution(x1, x2, x3, x4):     return (x1 | x2) &amp; (x3 | x4)  |와 &amp;의 정의를 해야겠다.  ","categories": ["programmers"],
+        "tags": [],
+        "url": "/programmers/%EA%B0%84%EB%8B%A8%ED%95%9C-%EB%85%BC%EB%A6%AC-%EC%97%B0%EC%82%B0/",
+        "teaser": null
+      },{
+        "title": "[프로그래머스] 글자 이어 붙이기",
+        "excerpt":"문자 설명     문자열 my_string과 정수 배열 ‘index_list’가 매개변수로 주어집니다. my_string의 index_list의 원소들에 해당하는 인덱스의 글자들을 순서대로 이어 붙인 문자열을 return 하는 solution 함수를 작성해 주세요.    제한사항          1 ≤ my_string의 길이 ≤ 1,000     my_string의 원소는 영소문자로 이루어져 있습니다.     1 ≤ index_list의 길이 ≤ 1,000     0 ≤ index_list의 원소 &lt; my_string의 길이      📌my solution  def solution(my_string, index_list):     new = []     for i in index_list:         new.append(my_string[i])     return ''.join(new)    💕other solution  def solution(my_string, index_list):     return ''.join(my_string[idx] for idx in index_list)  ","categories": ["programmers"],
+        "tags": ["프로그래머스","python"],
+        "url": "/programmers/%EA%B8%80%EC%9E%90-%EC%9D%B4%EC%96%B4-%EB%B6%99%EC%97%AC-%EB%AC%B8%EC%9E%90%EC%97%B4-%EB%A7%8C%EB%93%A4%EA%B8%B0/",
+        "teaser": null
+      },{
+        "title": "[프로그래머스]주사위 게임 3",
+        "excerpt":"문제설명     1부터 6까지 숫자가 적힌 주사위가 네 개 있습니다. 네 주사위를 굴렸을 때 나온 숫자에 따라 다음과 같은 점수를 얻습니다.          네 주사위에서 나온 숫자가 모두 p로 같다면 1111 x p점을 얻습니다.     세 주사위에서 나온 숫자가 p로 같고 나머지 다른 주사위에서 나온 숫자가 q(p ≠ q)라면 (10 × p + q)2 점을 얻습니다.                                             주사위가 두 개씩 같은 값이 나오고, 나온 숫자를 각각 p, q(p ≠ q)라고 한다면 (p + q) ×             p - q             점을 얻습니다.                                     어느 두 주사위에서 나온 숫자가 p로 같고 나머지 두 주사위에서 나온 숫자가 각각 p와 다른 q, r(q ≠ r)이라면 q × r점을 얻습니다.     네 주사위에 적힌 숫자가 모두 다르다면 나온 숫자 중 가장 작은 숫자 만큼의 점수를 얻습니다.  네 주사위를 굴렸을 때 나온 숫자가 정수 매개변수 a, b, c, d로 주어질 때, 얻는 점수를 return 하는 solution 함수를 작성해 주세요.      제한 사항     a, b, c, d는 1 이상 6 이하의 정수 입니다.   def solution(a,b,c,d):     all_num = [a, b, c, d]     counts = [all_num.count(i) for i in all_num]     #각 수가 들어가면서 해당 원소가 몇 개 겹치는지 그 수를 넣어준다.     if max(counts) == 4: #만약 다 같은 수라면 counts = [4,4,4,4]이다.         return a * 1111     elif max(counts) == 3:         p = all_num[counts.index(3)]         q = all_num[counts.index(1)]         return (10 * p + q) ** 2     elif max(counts) == 2:         if min(counts) ==2:             return (a + c) * abs(a - c) if a == b else (a + b) * abs(a - b)         else:             p = all_num[counts.index(2)]             return (a * b * c * d) / p ** 2     else:         return min(all_num)    너무 어려웠다..ㅠㅠ풀다가 이 한 문제로 시간을 엄청 소비하고 있다는것을 알게됐다. 그래서 답을 보고 정의하고 다시 풀어보기로 했다. 꼭 다시 풀어봐야지…           abs 절대값을 구하는 것이다. abs(실수)     count count는 겹치는 숫자를 말해준다. 리스트.count(찾는 값) 리스트안에 찾는 값을 넣어주면 그 안에서 몇개가 겹치는지 나온다.     index() 안에 값을 넣으면 그 값이 있는 순서를 출력해준다.     ","categories": ["programmers"],
+        "tags": [],
+        "url": "/programmers/%EC%A3%BC%EC%82%AC%EC%9C%84-%EA%B2%8C%EC%9E%84-3/",
+        "teaser": null
+      }]
