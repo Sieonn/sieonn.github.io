2eb2c0b6c98293749df9d5eddd0cad61374ea8e8
sieon <sieon.eve@gmail.com>
2023-11-09 18:02:42 +0900

    Day04

--- _posts/프로그래머스/Lv.0/test.py
+++ _posts/프로그래머스/Lv.0/test.py
@@ -896,16 +896,18 @@
 
 # print(solution([1, 1, 2, 2]))
 
-def solution(array):
-    while len(array) != 0:
-        for i, a in enumerate(set(array)):
-            array.remove(a)
-        print(array, i)
-        if i == 0:
-            return a
-    return -1
+# def solution(array):
+#     while len(array) != 0:
+#         for i, a in enumerate(set(array)):
+#             array.remove(a)
+#         print(array, i)
+#         if i == 0:
+#             return a
+#     return -1
 
 
-array = [1, 2, 3, 3, 3, 4]
-print(set(array))
-print(solution(array))
+# array = [1, 2, 3, 3, 3, 4]
+# print(set(array))
+# print(solution(array))
+
+print(round(15//7))
