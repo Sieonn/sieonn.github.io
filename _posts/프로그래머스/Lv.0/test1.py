a0b59ae9bf2188dd1d9f13baa9c43dcfd8a7b9cf
Sieon <sieon.eve@gmail.com>
2023-11-24 14:05:28 +0900

    프로그래머스 13 완

--- _posts/프로그래머스/Lv.0/test1.py
+++ _posts/프로그래머스/Lv.0/test1.py
@@ -1,20 +1,50 @@
-T = int(input())
-for tc in range(1, T+1):
-    N = int(input())
-    value = []
-    for i in range(N):
-        A = input().split()
-        for j in range(int(A[1])):
-            value.append(A[0])
-    print(value)
+# T = int(input())
+# for tc in range(1, T+1):
+#     N = int(input())
+#     value = []
+#     for i in range(N):
+#         A = input().split()
+#         for j in range(int(A[1])):
+#             value.append(A[0])
+#     print(value)
     
-    Sten = ""
-    ten = []
-    for l, k in enumerate(value):
-        Sten += k
-        if len(Sten) == 10:
-            ten.append(Sten)
-            Sten = ""
-        elif l == len(value)-1:
-            ten.append(Sten)
-    print(f'#{tc}',*ten, sep= "\n")       
+#     Sten = ""
+#     ten = []
+#     for l, k in enumerate(value):
+#         Sten += k
+#         if len(Sten) == 10:
+#             ten.append(Sten)
+#             Sten = ""
+#         elif l == len(value)-1:
+#             ten.append(Sten)
+#     print(f'#{tc}',*ten, sep= "\n")       
+# def solution(s):
+#     s = list(s.split())
+#     print(s)
+#     value = 0
+#     for i, v in enumerate(s):
+#         if v == "Z":
+#             value -= int(s[i-1])
+#         else:
+#             value += int(v)
+#     return value
+            
+
+# s = "-1 -2 -3 Z"
+# solution(s)
+
+# def solution(my_string):
+#     new = ''
+#     for i in my_string:
+#         if i not in new:
+#             new += i
+#     return new
+# print(solution("people"))
+
+def solution(sides):
+    sides = sorted(sides)
+    if sides[2]< sides[0] + sides[1]: 
+        return 1
+    else:
+        return 2
+print(solution([199, 72, 222]))
