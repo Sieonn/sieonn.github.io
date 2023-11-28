d6a450bc3e03be2691baa6920f186222a174fa3e
Sieon <sieon.eve@gmail.com>
2023-11-28 21:36:51 +0900

    1940완

--- _posts/프로그래머스/Lv.0/swea.py
+++ _posts/프로그래머스/Lv.0/swea.py
@@ -1,14 +1,35 @@
 #1945
+# T = int(input())
+# for tc in range(1, T+1):
+    # N = int(input())
+    # numbers = [2, 3, 5, 7, 11]
+    # count = [0]*5
+    # d = 0
+    # while d < 5:
+        # if N % numbers[d] == 0:
+            # N = N // numbers[d]
+            # count[d] += 1
+        # else:
+            # d += 1
+    # print(f'#{tc}',*count)
+    
 T = int(input())
 for tc in range(1, T+1):
-    N = int(input())
-    numbers = [2, 3, 5, 7, 11]
-    count = [0]*5
-    d = 0
-    while d < 5:
-        if N % numbers[d] == 0:
-            N = N // numbers[d]
-            count[d] += 1
-        else:
-            d += 1
-    print(f'#{tc}',*count)
+    answer = 0
+    now = 0
+    for i in range(int(input())):
+        speed = list(map(int,input().split()))
+        # print(speed)
+        if speed[0] == 1:
+            now = now + speed[1]
+            answer += now
+        elif speed[0] == 2:
+            now = now - speed[1]
+            if now < 0:
+                now = 0
+            else:
+                answer += now 
+        elif speed[0] == 0:
+            answer += now
+    print(f'#{tc}', answer)
+                
